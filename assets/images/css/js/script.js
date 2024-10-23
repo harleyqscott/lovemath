51df2ca71b48b0115f434ec3970e5713673e2c62
Harley Scott <harley.scott@students.codeinstitute.net>
2024-10-23 09:53:05 +0000

    added enter key submit

--- assets/images/css/js/script.js
+++ assets/images/css/js/script.js
@@ -14,9 +14,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
             }    
-        });
+        })
 
     }
+
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
 
     runGame("addition");
 });
